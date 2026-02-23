cf5707b59908e02b032c84e1ab6125b0a3925c33
adil <adil.business4064@gmail.com>
2026-02-24 04:47:08 +0530

    instruction:close_market.rs account struct added

---
+++ programs/cinefi/src/instructions/close_market.rs
@@ -0,0 +1,20 @@
+use anchor_lang::prelude::*;
+use crate::{errors::ErrorCode, states::*};
+
+#[derive(Accounts)]
+pub struct CloseMarket<'info>{
+  #[account(mut)]
+  pub caller: Signer<'info>,
+
+
+  #[account(
+    mut,
+    seeds=[MARKET_SEED, &market.key().as_ref()],
+    bump,
+    constraint = !market.closed
+      @ErrorCode::MarketAlreadyClosed,
+    constraint = Clock::get()?.unix_timestamp >= market.betting_closes_at
+      @ErrorCode::BettingStillOpen
+  )]
+  pub market: Account<'info, Market>
+}
