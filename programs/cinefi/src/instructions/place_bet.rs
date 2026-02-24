3453208c6e517216280d3c2384266f31bf5202f7
adil <adil.business4064@gmail.com>
2026-02-25 02:55:36 +0530

    states:place_bet.rs instruction logic added

--- programs/cinefi/src/instructions/place_bet.rs
+++ programs/cinefi/src/instructions/place_bet.rs
@@ -1,7 +1,48 @@
 use anchor_lang::prelude::*;
 use crate::{errors::ErrorCode, states::*};
+use anchor_lang::system_program::{transfer, Transfer};
+use crate::utils::*;
 
 pub fn place_bet(ctx:Context<PlaceBet>, bucket: u8, amount: u64)->Result<()>{
+  let clock=Clock::get()?;
+  let now=clock.unix_timestamp;
+
+  let market=&mut ctx.accounts.market;
+  let user_position=&mut ctx.accounts.user_position;
+
+  require!(amount > MINIMUM_STAKE_AMOUNT , ErrorCode::InvalidAmount);
+  require!(!market.closed, ErrorCode::MarketAlreadyClosed);
+  require!(!market.resolved, ErrorCode::MarketAlreadyResolved);
+  require!(now <= market.betting_closes_at, ErrorCode::BettingClosed);
+  require!(bucket>0 && bucket<=100, ErrorCode::InvalidBucket);
+
+  let day_index=get_day_index(now,market.betting_starts_at);
+  let new_weighted_amount=calc_weigthed_amount(amount,day_index)?;
+
+  let cpi_context=CpiContext::new(
+    ctx.accounts.system_program.to_account_info(),
+    Transfer{
+      from:ctx.accounts.user.to_account_info(),
+      to:ctx.accounts.vault.to_account_info()
+    }
+  );
+  transfer(cpi_context, amount)?;
+
+  let idx= bucket as usize;
+  market.pool[idx]=market.pool[idx].checked_add(amount).ok_or(ErrorCode::MathOverflow)?;
+  market.weighted_pool[idx]=market.weighted_pool[idx].checked_add(new_weighted_amount).ok_or(ErrorCode::MathOverflow)?;
+  market.total_pool=market.total_pool.checked_add(amount).ok_or(ErrorCode::MathOverflow)?;
+
+  if user_position.amount==0 {
+    user_position.user=ctx.accounts.user.key();
+    user_position.market=market.key();
+    user_position.bucket=bucket;
+    user_position.claimed=false;
+    user_position.bump=ctx.bumps.user_position;
+  }
+  user_position.amount=user_position.amount.checked_add(amount).ok_or(ErrorCode::MathOverflow)?;
+  user_position.weighted_amount=user_position.weighted_amount.checked_add(new_weighted_amount).ok_or(ErrorCode::MathOverflow)?;
+
   Ok(())
 }
 
