34d0f021cd7f12e01fdf385f727b9eb5d681e5c4
adil <adil.business4064@gmail.com>
2026-02-25 04:09:10 +0530

    instruction:resolve_market.rs instruction logic added

--- programs/cinefi/src/instructions/resolve_market.rs
+++ programs/cinefi/src/instructions/resolve_market.rs
@@ -1,8 +1,56 @@
 use anchor_lang::prelude::*;
+use anchor_lang::system_program::{Transfer, transfer};
 use crate::states::*;
 use crate::errors::ErrorCode;
+use crate::utils::*;
 
 pub fn resolve_market(ctx:Context<ResolveMarket>)->Result<()>{
+  let market=&mut ctx.accounts.market;
+  let oracle_report=&ctx.accounts.oracle_report;
+
+  require!(market.closed, ErrorCode::MarketNotClosed);
+  require!(!market.resolved, ErrorCode::MarketAlreadyResolved);
+  require!(oracle_report.finalized, ErrorCode::OracleNotFinalized);
+  require!(!oracle_report.disputed, ErrorCode::OracleDisputed);
+
+  let final_outcome=oracle_report.agreed_score;
+
+  let fee=(market.total_pool as u128).checked_mul(DEFAULT_PROTOCOL_FEE_BPS as u128).ok_or(ErrorCode::MathOverflow)?/10_000u128;
+  let fee= fee as u64;
+
+  let total_prize_pool=market.total_pool.checked_sub(fee).ok_or(ErrorCode::MathOverflow)?;
+
+  if fee > 0 {
+    let vault_bump = ctx.bumps.vault;
+    let market_key = market.key();
+
+    let market_key_bytes = market_key.as_ref();
+    let bump_seed = &[vault_bump];
+    let vault_seeds: &[&[u8]] = &[VAULT_SEED, market_key_bytes, bump_seed];
+
+    let signer_seeds: &[&[&[u8]]] = &[vault_seeds];
+
+    transfer(
+      CpiContext::new_with_signer(
+        ctx.accounts.system_program.to_account_info(),
+        Transfer{
+          from:ctx.accounts.vault.to_account_info(),
+          to:ctx.accounts.treasury.to_account_info()
+        },
+        signer_seeds
+      ),
+      fee
+    )?;
+  }
+
+  let (bucket_prizes, fallback_used)=compute_bucket_prizes(&market.weighted_pool,final_outcome,market.radius,total_prize_pool);
+
+  market.bucket_prize=bucket_prizes;
+  market.final_outcome=final_outcome;
+  market.total_prize_pool=total_prize_pool;
+  market.fallback_used=fallback_used;
+  market.resolved=true;
+
   Ok(())
 }
 
