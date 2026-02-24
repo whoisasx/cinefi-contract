33d54b259ff9cf46b4bb9f8cb29b3a36fe19bfbb
adil <adil.business4064@gmail.com>
2026-02-24 05:52:41 +0530

    instruction:reclaim_pool.rs accounts struct added

---
+++ programs/cinefi/src/instructions/reclaim_pool.rs
@@ -0,0 +1,38 @@
+use anchor_lang::prelude::*;
+use crate::states::*;
+use crate::errors::ErrorCode;
+
+#[derive(Accounts)]
+pub struct ReclaimPool<'info>{
+  #[account(mut)]
+  pub caller: Signer<'info>,
+
+  #[account(
+    mut,
+    seeds=[MARKET_SEED,&market.media_id.to_le_bytes()],
+    bump=market.bump,
+    constraint= market.resolved
+      @ErrorCode::MarketNotResolved,
+    constraint = !market.reclaimed
+      @ErrorCode::MarketAlreadyClaimed,
+    constraint= Clock::get()?.unix_timestamp >= market.claim_deadline
+      @ErrorCode::ClaimDeadlineNotPassed
+  )]
+  pub market: Account<'info ,Market>,
+
+  #[account(
+    mut,
+    seeds=[VAULT_SEED, market.key().as_ref()],
+    bump
+  )]
+  pub vault: UncheckedAccount<'info>,
+
+  #[account(
+    mut,
+    seeds=[TREASURY_SEED],
+    bump
+  )]
+  pub treasury: UncheckedAccount<'info>,
+
+  pub system_program: Program<'info,System>
+}
