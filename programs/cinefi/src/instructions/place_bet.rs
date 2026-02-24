d8c8f834535b18ec49fa4b45ef325dc11e1f6a43
adil <adil.business4064@gmail.com>
2026-02-24 05:40:03 +0530

    instruction:place_bet.rs accounts struct added

---
+++ programs/cinefi/src/instructions/place_bet.rs
@@ -0,0 +1,44 @@
+use anchor_lang::prelude::*;
+use crate::{errors::ErrorCode, states::*};
+
+#[derive(Accounts)]
+#[instruction(bucket: u8, amount: u64)]
+pub struct PlaceBet<'info>{
+  #[account(mut)]
+  pub user: Signer<'info>,
+
+  #[account(
+    mut,
+    seeds=[MARKET_SEED, &market.media_id.to_le_bytes()],
+    bump = market.bump,
+    constraint = !market.resolved
+      @ErrorCode::MarketAlreadyResolved,
+    constraint = !market.closed
+      @ErrorCode::MarketAlreadyClosed,
+    constraint = Clock::get()?.unix_timestamp >=market.betting_starts_at
+      @ErrorCode::BettingNotStarted,
+    constraint = Clock::get()?.unix_timestamp <= market.betting_closes_at
+      @ErrorCode::BettingClosed
+  )]
+  pub market: Account<'info, Market>,
+
+  #[account(
+    init_if_needed,
+    payer = user,
+    space = 8+UserPosition::INIT_SPACE,
+    seeds = [POSITION_SEED, user.key().as_ref(), market.key().as_ref(), &[bucket]],
+    bump,
+    constraint = user_position.user == Pubkey::default() || user_position.user == user.key() 
+      @ErrorCode::Unauthorized
+  )]
+  pub user_position: Account<'info, UserPosition>,
+
+  #[account(
+    mut,
+    seeds=[VAULT_SEED, market.key().as_ref()],
+    bump
+  )]
+  pub vault: UncheckedAccount<'info>,
+
+  pub system_program: Program<'info, System>
+}
