f8732fd061d92cd1a72e75173fcb95e7942e5ea5
adil <adil.business4064@gmail.com>
2026-03-04 16:29:31 +0530

    src:states/. minor bug fix

--- programs/cinefi/src/states/oracle_report.rs
+++ programs/cinefi/src/states/oracle_report.rs
@@ -1,11 +1,17 @@
 use anchor_lang::prelude::*;
 
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy, Default, InitSpace)]
+pub struct OracleSubmission {
+  pub signer: Pubkey,
+  pub score: u8,
+}
+
 #[account]
 #[derive(InitSpace)]
 pub struct OracleReport{
   pub market: Pubkey,
 
-  pub submissions: [(Pubkey,u8);3],
+  pub submissions: [OracleSubmission; 3],
   pub submission_count: u8,
 
   pub agreed_score: u8,
